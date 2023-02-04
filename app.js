d4bb9a9dd48b783aaa0dec2987a60887ee5ee2cf
Din <Dinz5005@gmail.com>
2023-02-04 21:10:25 +0200

    finish_project

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const source = document.querySelector("picture source");
 const dot_1 = document.querySelector(".state_1");
 const dot_2 = document.querySelector(".state_2");
 
-// Desktop
-setInterval(() => {
+const desktopInterval = () => {
+  if (window.screen.width <= 480) {
+    clearInterval(desktopInterval);
+    setInterval(mobileInterval, 4000);
+  }
   if (img.alt === "Desktop_Img") {
     img.src = "./assets/main_top_banner/desktop2.jpg";
     dot_1.style.backgroundColor = "transparent";
@@ -16,14 +19,23 @@ setInterval(() => {
       dot_1.style.backgroundColor = "white";
     }, 2000);
   }
-}, 4000);
+};
 
-// Mobile
-setInterval(() => {
+const mobileInterval = () => {
+  if (window.screen.width > 480) {
+    clearInterval(mobileInterval);
+    setInterval(desktopInterval, 4000);
+  }
   if (source.srcset === "./assets/main_top_banner/mobile.jpg") {
     source.srcset = "./assets/main_top_banner/mobile2.jpg";
     setTimeout(() => {
       source.srcset = "./assets/main_top_banner/mobile.jpg";
     }, 2000);
   }
-}, 4000);
+};
+
+if (window.screen.width > 480) {
+  setInterval(desktopInterval, 4000);
+} else {
+  setInterval(mobileInterval, 4000);
+}
